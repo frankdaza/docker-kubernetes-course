bee0a2dc8910a749896d5d301f27c6e3ad19b62d
Frank Edward Daza González <frank.daza2@gmail.com>
2020-07-30 21:08:37 -0500

    App Server Starter Code.

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-   res.send('Hi there Knarf!');
+   res.send('How are you doing  Knarf!');
 });
 
 app.listen(8080, () => {

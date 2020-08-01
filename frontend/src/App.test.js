ee5895bf64bbe23ba0808b78ba3190c20ae86f73
Frank Edward Daza González <frank.daza2@gmail.com>
2020-08-01 17:27:16 -0500

    Executing Tests. Fix test.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('hi there knarf', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hi there Knarf!/i);
   expect(linkElement).toBeInTheDocument();
 });

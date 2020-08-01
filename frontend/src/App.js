a5948f28a5b1c8ee26434955c1e02e191ac0baca
Frank Edward Daza González <frank.daza2@gmail.com>
2020-08-01 17:02:35 -0500

    Shorthand with Docker Compose.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there Knarf!
+          Hi there Chango Man!
         </p>
       </header>
     </div>
